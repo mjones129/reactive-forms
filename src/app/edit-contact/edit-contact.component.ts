a5e6ea99d08cac4be6d77a3473e78306f979c079
Matt <me@mattjones.tech>
2025-03-14 16:26:01 -0400

    add form controls to name fields

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms'
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
+  firstName = new FormControl();
+  lastName = new FormControl();
+  dateOfBirth = new FormControl();
+  favoritesRanking = new FormControl();
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -14,6 +20,9 @@ export class EditContactComponent implements OnInit {
   }
 
   saveContact() {
-
+    console.log(this.firstName.value);
+    console.log(this.lastName.value);
+    console.log(this.dateOfBirth.value);
+    console.log(this.favoritesRanking.value);
   }
 }
