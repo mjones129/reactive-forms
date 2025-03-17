294d46e0af203007ee0092c2d478fd3c13a29705
Matt <me@mattjones.tech>
2025-03-17 15:29:15 -0400

    custom input control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { ContactListComponent } from './contact-list/contact-list.component';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
 import { InMemoryContactsApi } from './contacts/in-memory-contacts.service';
 import { DateValueAccessorDirective } from './date-value-accessor/date-value-accessor.directive';
+import { ProfileIconSelectorComponent } from './profile-icon-selector/profile-icon-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
     EditContactComponent,
-    DateValueAccessorDirective
+    DateValueAccessorDirective,
+    ProfileIconSelectorComponent
   ],
   imports: [
     BrowserModule,
