41a612eb875ceef5912d23a9d046c63d721b0f12
Matt <me@mattjones.tech>
2025-03-17 07:45:44 -0400

    add radio buttons from a data source

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms'
 import { ContactsService } from '../contacts/contacts.service';
+import { phoneTypeValues } from '../contacts/contact.model';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  contactForm = new FormGroup({
-    id : new FormControl(),
-    firstName : new FormControl(),
-    lastName : new FormControl(),
-    dateOfBirth : new FormControl(),
-    favoritesRanking : new FormControl(),
-    phone: new FormGroup({
-      phoneNumber: new FormControl(),
-      phoneType: new FormControl()
+  phoneTypes = phoneTypeValues;
+  contactForm = this.fb.nonNullable.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null>null,
+    favoritesRanking: <number | null>null,
+    phone: this.fb.nonNullable.group({
+      phoneNumber: '',
+      phoneType: ''
     }),
-    address: new FormGroup({
-      streetAddress: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      postalCode: new FormControl(),
-      addressType: new FormControl()
+    address: this.fb.nonNullable.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: ''
     }),
   });
 
-  constructor(private route: ActivatedRoute, private contactService:ContactsService, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private contactService:ContactsService,
+    private router: Router,
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
@@ -35,19 +42,7 @@ export class EditContactComponent implements OnInit {
 
     this.contactService.getContact(contactId).subscribe((contact) => {
       if (!contact) return;
-      
-      this.contactForm.controls.id.setValue(contact.id);
-      this.contactForm.controls.firstName.setValue(contact.firstName);
-      this.contactForm.controls.lastName.setValue(contact.lastName);
-      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking);
-      this.contactForm.controls.phone.controls.phoneNumber.setValue(contact.phone.phoneNumber);
-      this.contactForm.controls.phone.controls.phoneType.setValue(contact.phone.phoneType);
-      this.contactForm.controls.address.controls.streetAddress.setValue(contact.address.streetAddress);
-      this.contactForm.controls.address.controls.city.setValue(contact.address.city);
-      this.contactForm.controls.address.controls.state.setValue(contact.address.state);
-      this.contactForm.controls.address.controls.postalCode.setValue(contact.address.postalCode);
-      this.contactForm.controls.address.controls.addressType.setValue(contact.address.addressType);
+      this.contactForm.setValue(contact); 
     });
   }
 
