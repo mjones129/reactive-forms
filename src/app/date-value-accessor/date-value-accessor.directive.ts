7d5999dce3643856751eda30e188a574cd7a151f
Matt <me@mattjones.tech>
2025-03-17 14:18:29 -0400

    format when form changes

--- src/app/date-value-accessor/date-value-accessor.directive.ts
+++ src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, Provider, forwardRef } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Directive, ElementRef, Provider, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 const DATE_VALUE_PROVIDER: Provider = {
   provide: NG_VALUE_ACCESSOR,
@@ -11,8 +11,16 @@ const DATE_VALUE_PROVIDER: Provider = {
   selector: 'input([type=date])[formControlName],input([type=date])[formControl],input([type=date])[ngModel]',
   providers: [DATE_VALUE_PROVIDER]
 })
-export class DateValueAccessorDirective {
 
-  constructor() { }
+export class DateValueAccessorDirective implements ControlValueAccessor {
+
+  constructor(private element: ElementRef) { }
+
+  writeValue(newValue:any) {
+    //yyyy-mm-dd
+    if(newValue instanceof Date) {
+      this.element.nativeElement.value = newValue.toISOString().split('T')[0];
+    }
+  }
 
 }
